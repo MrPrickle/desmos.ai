cd7986e3425b3c559a24ea40dd7e9f2d65aa551d
Willliam L <73481552+MrPrickle@users.noreply.github.com>
2024-05-28 10:39:35 -0400

    Added basic AI Model

--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
-print("Hi")
-print("My first commit")
+from openai import OpenAI
+client = OpenAI()
+
+response = ""
+
+response = client.images.generate(
+  model = "dall-e-3",
+  prompt = "gyatt recoil",
+  size = "1024x1024",
+  quality = "standard",
+  n = 1,
+)
+
+image_url = response.data[0].url
