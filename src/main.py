9ef64d5fe11425387047433995fcc86fdb9e69c4
William Li <rayford.li@gmail.com>
2024-05-30 09:57:19 -0400

    I tried json stuff

--- src/main.py
+++ src/main.py
@@ -4,25 +4,41 @@ import json
 url = "https://modelslab.com/api/v6/images/text2img"
 
 payload = json.dumps({
-    "key": "api-key",
+	"key": "",
     "model_id": "ae-sdxl-v1",
-    "prompt": "ultra realistic close up portrait ((beautiful pale female with heavy black eyeliner and big boob and big ass thicc thighs)), blue eyes, shaved side haircut, hyper detail, cinematic lighting, magic neon, dark red city, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K",
+    "prompt": "ultra realistic close up portrait ((A smart and handsome man named leo)), brown eyes, shaved side haircut, hyper detail, cinematic lighting, magic neon, dark red city, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K",
     "negative_prompt": "painting, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, deformed, ugly, blurry, bad anatomy, bad proportions, extra limbs, cloned face, skinny, glitchy, double torso, extra arms, extra hands, mangled fingers, missing lips, ugly face, distorted face, extra legs, anime",
     "width": "512",
     "height": "512",
     "samples": "1",
     "num_inference_steps": "30",
-    "seed": null,
+    "safety_checker": "no",
+    "enhance_prompt": "yes",
+    "seed": 56,
     "guidance_scale": 7.5,
+    "multi_lingual": "no",
+    "panorama": "no",
+    "self_attention": "no",
+    "upscale": "no",
+    "embeddings_model": None,
+    "lora_model": None,
+    "tomesd": "yes",
+    "use_karras_sigmas": "yes",
+    "vae": None,
+    "lora_strength": None,
     "scheduler": "UniPCMultistepScheduler",
-    "webhook": null,
-    "track_id": null
+    "webhook": None,
+    "track_id": None
 })
 
 headers = {
-  'Content-Type': 'application/json'
+    'Content-Type': 'application/json'
 }
 
 response = requests.request("POST", url, headers=headers, data=payload)
 
+response_data = response.json
+
 print(response.text)
+
+print(json.dumps(response_data, indent=4))
