69317ea5eb0c52d709a0b5eb5f5bcbae95ded0d5
Willliam <73481552+WillBillDillPickle@users.noreply.github.com>
2024-05-31 22:00:09 -0400

    tried

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
-# Text to Image
+from diffusers import DiffusionPipeline
 
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 
-# Image to Black and White
+from PIL import Image
 
+model_id = "stabilityai/stable-diffusion-2"
+scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
 
-# Image outline
+prompt = "a photo of an astronaut riding a horse on mars"
+image = pipe(prompt).images[0]
 
-
-# To desmos
+image.save("generated_image.png")
