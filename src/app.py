8ef0ee8df09d01c1aebe9882d6b7bab8e168d001
William Li <rayford.li@gmail.com>
2024-06-03 09:58:02 -0400

    hbkjnl

--- src/app.py
+++ src/app.py
@@ -1,10 +1,16 @@
 import sys
 import os
 
-script_dir = os.path.dirname(os.path.abspath('C:/Users/Riche/OneDrive/Desktop/Documents/.github/desmos.ai/src/decor.py'))
 
-sys.path.insert(0, 'C:/Users/Riche/OneDrive/Desktop/Documents/.github/desmos.ai/src/decor.py')  # Add the script's directory to the Python path
-sys.path.insert(1, 'C:/Users/Riche/OneDrive/Desktop/Documents/.github/desmos.ai/src/main.py')  # Add the script's directory to the Python path
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+scripts_dir = os.path.join(current_dir, 'src')
+
+sys.path.insert(0, scripts_dir)
+
+# sys.path.insert(0, 'C:/Users/Riche/OneDrive/Desktop/Documents/.github/desmos.ai/src/decor.py')  # Add the script's directory to the Python path
+# sys.path.insert(1, 'C:/Users/Riche/OneDrive/Desktop/Documents/.github/desmos.ai/src/main.py')  # Add the script's directory to the Python path
 
 import decor
 import main
